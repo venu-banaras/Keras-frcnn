faad13b779b3cdcba5e057bcde42acc5e45d04eb
Mayank Singh <venu.banaras@gmail.com>
2023-04-11 15:11:20 +0530

    Update losses.py

--- keras_frcnn/losses.py
+++ keras_frcnn/losses.py
@@ -1,8 +1,8 @@
-# from keras import backend as K
-import keras.backend as K
+from keras import backend as K
+# import keras.backend as K
 from keras.metrics import categorical_crossentropy
 
-if K.common.image_dim_ordering() == 'tf':
+if K.image_data_format() == 'tf':
 	import tensorflow as tf
 
 lambda_rpn_regr = 1.0
